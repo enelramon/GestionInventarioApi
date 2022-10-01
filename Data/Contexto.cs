41a7dec036e5eacbf85cc4b56bc6f844ae73ed12
Enel Almonte <enel_almonte@ucne.edu.do>
2022-09-30 21:35:31 -0400

    Agregando primer controller

---
+++ Data/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using GestionInventarioApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionInventarioApi.Data
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+        public DbSet<Articulos> Articulos { get; set; }
+    }
+}
